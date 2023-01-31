44daca91457e9053c6c3315213c2142f3144da55
Arielle <arhancko@gmail.com>
2023-01-30 22:05:51 -0800

    new

--- 24gc.py
+++ 24gc.py
@@ -5,8 +5,15 @@
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
 
+GC = 0.0
+for i in range(len(dna)):
+	if dna[i] == 'G' or dna[i] == 'C': GC = GC +1
+	
+a = GC / len(dna)
+print(f'{a:.2f}')
 
 """
 python3 24gc.py
 0.42
+add together all occurrances of G and C then divide that by the length
 """
