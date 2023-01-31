912a68936c6307044a660421c4876674754d54c2
Arielle <arhancko@gmail.com>
2023-01-30 22:06:31 -0800

    new

--- 26anti.py
+++ 26anti.py
@@ -7,8 +7,23 @@
 
 dna = 'ACTGAAAAAAAAAAA'
 
+s = '' #creates an empty string
+for i in range(len(dna)): #all of this stores the compl. base to each given base
+	if dna[i] == 'A': 
+		s = s + 'T'
+	elif dna[i] == 'T': 
+		s = s + 'A'
+	elif dna[i] == 'C': 
+		s = s + 'G'
+	else: s = s +'C'
+	
+print(s[::-1]) #prints the string but backwards
 
 """
 python3 26anti.py
 TTTTTTTTTTTCAGT
+
+ideas:
+make an empty string and if A then store T, etc.
+
 """
